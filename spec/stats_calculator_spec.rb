6ddd8cf6f38dac4fbccd9436849cb51d2f69f765
Enrique Temes <enrtemes@gmail.com>
2011-10-10 22:37:43 -0400

    updated stats_calculator specs

--- spec/stats_calculator_spec.rb
+++ spec/stats_calculator_spec.rb
@@ -14,7 +14,7 @@ describe "Brock::StatsCalculator" do
   # Kirby Puckett 1987 Stats
   let(:valid_stats) { { :games => 157, :at_bats => 624, :runs => 96, :hits => 207, :doubles => 32, :triples => 5, :home_runs => 28, :rbi => 99, :sb => 12, :cs => 7, :walks => 32, :strike_outs => 91, :gidp => 16, :hbp => 6, :sh => 0, :sf => 6, :iw => 7 } }
 
-  let(:invalid_stats) { { :year => 2000, :hits => 150, :doubles => 20, :triples => 1, :home_runs => 20} }
+  let(:invalid_stats) { { :at_bats => 200, :games => 150, :doubles => 20, :triples => 1, :home_runs => 20, :walks => 10, :sb => 1, :cs => 2, :gidp => 5} }
 
   let(:zero_stats) { { :games => 0, :at_bats => 0, :runs => 0, :hits => 0, :doubles => 0, :triples => 0, :home_runs => 0, :rbi => 0, :sb => 0, :cs => 0, :walks => 0, :strike_outs => 0, :gidp => 0, :hbp => 0, :sh => 0, :sf => 0, :iw => 0 } }
 
@@ -25,7 +25,7 @@ describe "Brock::StatsCalculator" do
     end
 
     it "should raise error when stats hash is missing any of the required keys or values" do
-      lambda{ Calculator.total_bases(invalid_stats) }.should raise_error(Brock::InvalidStatsHashError, "Stats hash must contain all elements with values: Missing element for games")
+      lambda{ Calculator.total_bases(invalid_stats) }.should raise_error(Brock::InvalidStatsHashError, "Stats hash must contain all elements with values: Missing element for hits")
     end
   end
 
@@ -39,7 +39,7 @@ describe "Brock::StatsCalculator" do
     end
 
     it "should raise error when stats hash is missing any of the required keys or values" do
-      lambda{ Calculator.runs_created(invalid_stats) }.should raise_error(Brock::InvalidStatsHashError, "Stats hash must contain all elements with values: Missing element for games")
+      lambda{ Calculator.runs_created(invalid_stats) }.should raise_error(Brock::InvalidStatsHashError, "Stats hash must contain all elements with values: Missing element for hits")
     end
   end
 
